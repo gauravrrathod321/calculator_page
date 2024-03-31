005589405266b4a58cc5059eba95964206581633
gauravrathod321 <gauravrathod321@gmail.com>
2024-03-31 18:02:36 +0530

    calculator wepage added

---
+++ index.js
@@ -0,0 +1,34 @@
+// console.log("hello javascript")
+// // console.log("hello javascript") 
+
+// calculate sum of 1 to 5
+
+// for (let count=1; count<=5;count++) {
+//     console.log("hello js");
+// }
+
+// let sum = 0;
+// for (let i=1;i<=5;i++){
+//     sum = sum+i; 
+// } 
+// console.log("sum=",sum);
+// // console.log("loop has ended") 
+
+// let mul = 1;
+// for (let i=1;i<=5;i++){
+//     mul = mul*i;
+// }
+// console.log("mul=",mul);
+// console.log("loop has ended") 
+
+// for (let i=1;i<=5;i++){
+//     console.log("i=",i)
+// }
+// let i=6
+// console.log(i) 
+
+function myfunction(msg) {
+    console.log(msg);
+    console.log(msg);
+}
+myfunction("i love js",100)
